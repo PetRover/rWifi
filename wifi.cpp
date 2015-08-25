9758427468860fbf52fd35e929d9596ade171a27
Bryce Cater <brycedcarter@gmail.com>
2015-08-25 18:05:25 -0400

    added base cpp and h files for each module

---
+++ wifi.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Bryce Cater on 8/25/15.
+//
+
+#include "wifi.h"
---
+++ wifi.h
@@ -0,0 +1,8 @@
+//
+// Created by Bryce Cater on 8/25/15.
+//
+
+#ifndef FIRMWARE_WIFI_H
+#define FIRMWARE_WIFI_H
+
+#endif //FIRMWARE_WIFI_H
